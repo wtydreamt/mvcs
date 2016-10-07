a8a90662abd5a21b75caf015a9a0a34686a9016e
unknown <>
2016-10-07 08:19:15 +0800

    remove imooc

--- imooc/core/config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-    'database_type' => 'mysql',
-    'database_name' => 'oct',
-    'server' => 'localhost',
-    'username' => 'root',
-    'password' => 'root',
-    'charset' => 'utf8',
-	);
--- imooc/log/2016092915/log.php
+++
@@ -1,5 +0,0 @@
-2016-09-29 15:50:33"SSS"
-2016-09-29 15:56:03"SSS"
-2016-09-29 15:56:03"ctrl:\\app\\ctrl\\indexCtrl\r\naction:index"
-2016-09-29 15:57:13"SSS"
-2016-09-29 15:57:13"ctrl:\\app\\ctrl\\indexCtrl\r\naction:index"
